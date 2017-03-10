ffd4886f6feab7ea1bc22440134be35d3bd22697
wjrobinson11 <wes@socialcompass.net>
2017-03-09 21:29:46 -0800

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/home', to: 'pages#home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
